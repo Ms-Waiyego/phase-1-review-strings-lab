8b4dc99a0f1ab67557b1f0d747b4b1a1a188fbc1
Brenda <waiyegobrenda99@gmail.com>
2022-08-16 23:53:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Waiyego Mburu!';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = 'Welcome, !';
